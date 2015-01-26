72a217a5ece4905232136477be8600bb45d54677
arvind <arvind@teddy.(none)>
2015-01-26 17:15:57 -0500

    Add screenshots for demonstration purposes

--- sample_code/test.c
+++ sample_code/test.c
@@ -1,14 +1,12 @@
 /* http://iamtgc.com/selectively-statically-compile-and-link-nix-binaries/ */
-
 #include <stdio.h>
 #include <math.h>
 
 void calc(){
-        float d= cos(90);
-        printf("square root of 4 is %f", d);
+    float d= cos(90);
+    printf("cosine of 90 is %f", d);
 }
 
 int main() {
     calc();
 }
-
